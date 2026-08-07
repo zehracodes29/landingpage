abfcd110bc3f1539fb0fc436789ef23e5e83a37e
binte zehra <rzehra942@gmail.com>
2026-08-07 13:45:25 +0530

    feat: configure Tailwind CSS and PostCSS for the project

--- pagemistri-landing/.next/dev/static/chunks/_219uq1s._.js
+++ pagemistri-landing/.next/dev/static/chunks/_219uq1s._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_tailwindcss_dist_1ejtfif._.js","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/src_1j05wv_._.css","static/chunks/_08cwztb._.js","static/chunks/node_modules_098t_2-._.js"],
+    chunks: ["static/chunks/src_1j05wv_._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/_08cwztb._.js","static/chunks/node_modules_098t_2-._.js"],
     source: "entry"
 });
--- pagemistri-landing/.next/server/app/_global-error.segments/_tree.segment.rsc
+++ pagemistri-landing/.next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"HDImXe4GdtN4myfcFwTPq"}
+0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"sWE-O51HgaiqmmeZ4aQt2"}
