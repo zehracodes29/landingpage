be746ae5d86909ae84d210c6452d58d20e6475ad
binte zehra <rzehra942@gmail.com>
2026-08-08 13:46:09 +0530

    feat: initialize Next.js project structure with layout and package configuration

--- pagemistri-landing/.next/dev/static/chunks/_219uq1s._.js
+++ pagemistri-landing/.next/dev/static/chunks/_219uq1s._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_index_144e4m2.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/_04kkm3q._.js","static/chunks/node_modules_0evoetu._.js"],
+    chunks: ["static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/_0zu-cge._.js"],
     source: "entry"
 });
